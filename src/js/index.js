99e0496423ef1e8aede82ff3ad1dd7b9413b75d7
biyuqi <biyuqiwan@163.com>
2016-12-08 09:38:29 +0800

    touchmove sidebar

--- src/js/index.js
+++ src/js/index.js
@@ -5,9 +5,16 @@ $(function(){
       index:0,
       num:0,
       items:$('.sidebar .item'),
+      siderBar:$('.sidebar'),
+      disX:0,//touchstart 的位置
+      disMoveX:0,//touchmove 的实时位置
+      disPercent:0,//移动百分比
+      speed:0,
+      flag:false,
       init:function(){
           this.clickBar();
           this.disBlock();
+          this.touchMovey();
       },
       //选项卡切换
       clickBar:function(){
@@ -20,14 +27,47 @@ $(function(){
           })
       },
       disBlock:function(){
+          var that = this;
           $('.btn').on('click',function () {
-              $('.sidebar').toggleClass('toogle');
+              $('.sidebar').animate({
+                  'left':'0'
+              },500);
           });
           $('.item').each(function (k,v) {
               $(v).click(function (e) {
                   e.stopPropagation();
                   $('.sidebar').removeClass('toogle');
               })
+          })
+      },
+      touchMovey:function(){
+          var that = this;
+          this.siderBar.on('touchstart',function(){
+              that.disX = event.touches[0].clientX;
+          });
+          this.siderBar.on('touchmove',function(){
+              that.disMoveX = event.touches[0].clientX;
+              that.disPercent = (that.disMoveX - that.disX)/640*100;
+              that.speed = that.disPercent + '%';
+              if(parseInt(that.speed) < 0){
+                  that.siderBar.css({
+                      'left':that.speed
+                  });
+              }
+              that.flag = true;
+              console.log(parseInt(that.disPercent))
+          });
+          this.siderBar.on('touchend',function(){
+                if(that.flag && parseInt(that.disPercent) < -20){
+                    that.siderBar.animate({
+                        'left':'-45%'
+                    },350);
+                }
+              if(that.flag && parseInt(that.disPercent) > -20 && parseInt(that.disPercent) < 0){
+                  that.siderBar.animate({
+                      'left':'0'
+                  },500);
+              }
           })
       }
   };
